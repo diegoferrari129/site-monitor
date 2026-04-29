644c48b35d1a93e9d6b83c6f06a5c0ba91e90d0c
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2026-04-29 18:57:06 +0200

    Aggiungere i file di progetto.

---
+++ SiteMonitor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SiteMonitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
